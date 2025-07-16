7c01b5c117c3b4e88073c5079768e57cf3c4c29f
Wamiq <wamiqzahid319@gmail.com>
2025-07-17 00:38:11 +0500

    Inital Registration DOne

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import landing_page, login_page
+from .views import landing_page, login_page, register_user_page, register_doctor_page
 
 urlpatterns = [
     path('', landing_page, name='landing'),
     path('login/', login_page, name='login'),
+    path('register/user/', register_user_page, name='register_user'),
+    path('register/doctor/', register_doctor_page, name='register_doctor'),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -7,3 +7,11 @@ def landing_page(request):
 
 def login_page(request):
     return render(request, 'auth/login.html')
+
+
+def register_user_page(request):
+    return render(request, 'auth/register/register_user.html')
+
+
+def register_doctor_page(request):
+    return render(request, 'auth/register/register_doctor.html')
