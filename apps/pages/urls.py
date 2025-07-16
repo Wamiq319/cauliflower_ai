0f85c6f78c3f222245ea5fed61f0fb55d3d699d7
Wamiq <wamiqzahid319@gmail.com>
2025-07-17 00:23:16 +0500

    Login age Added

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import landing_page
+from .views import landing_page, login_page
 
 urlpatterns = [
     path('', landing_page, name='landing'),
+    path('login/', login_page, name='login'),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def landing_page(request):
     return render(request, 'landing.html')
+
+
+def login_page(request):
+    return render(request, 'auth/login.html')
